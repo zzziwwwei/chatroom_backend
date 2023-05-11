a8a6de8a8ff09c2407db2862eb9e91562300c41a
zzziwwwei <ziwei000001@gmail.com>
2023-05-11 12:04:27 +0800

    test get/post res req

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
+const dotenv = require('dotenv');
+dotenv.config();
 const express = require('express');
+const {createPool} = require('mysql');
+const multer = require("multer");
 const app = express();
+app.use(express.urlencoded({ extended: true })); 
+app.use(express.json());
+app.use(multer().none()); 
+
+
+const databasePool = createPool({
+  host:process.env.MYSQL_HOST,
+  user:process.env.MYSQL_ROOT,
+  password:process.env.MYSQL_PASSWORD,
+  database:process.env.MYSQL_DATEBASE
+})
+
+
+
+
+
 const port = 3000;
 app.set('views', './views');
 app.set('view engine', 'ejs')
 app.use('/static', express.static(__dirname + '/public'));
+
+
+
+
 const { WebSocketServer } = require('ws');
 const wss = new WebSocketServer({ port: 8080 });
 wss.on('connection', function connection(ws) {
@@ -19,13 +43,25 @@ wss.on('connection', function connection(ws) {
 });
 
 app.get('/', (req, res) => {
-  // res.send('Hello World!')
   res.render('index', {
 
   })
 })
-app.post('/test', function (req, res) {
+app.get('/get', async function (req, res) {
+  console.log("get")
+  databasePool.query(`SELECT username FROM users;`,(err,res)=>{
+    return console.log(res);
+  })
   res.render('index')
+});
+
+app.post('/post', async function (req, res) {
+  console.log("post");
+  let username = req.body.username
+  let password = req.body.password
+  databasePool.query(`INSERT INTO users (username,password)
+  VALUES (?,?); `,[username, password]);
+  res.render('index');
 });
 app.listen(port, () => {
   console.log(port)
